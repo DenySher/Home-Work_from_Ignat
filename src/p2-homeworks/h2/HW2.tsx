d6f4c4a1751bf4d684b5a1aa6fbdda4661cec9c4
DenySher <d15m11@list.ru>
2022-07-31 21:07:20 +0300

    HW 2

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -1,39 +1,46 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Affairs from './Affairs'
 
 // types
-export type AffairPriorityType = any // need to fix any
-export type AffairType = any // need to fix any
-export type FilterType = 'all' | AffairPriorityType
+export type AffairPriorityType = 'high' | 'low' | 'middle'
+export type AffairType = {
+    _id: number,
+    name: string,
+    priority: string
+}
+export type FilterType = 'all' | AffairPriorityType // 'union type' объединяем типы
 
 // constants
-const defaultAffairs: any = [ // need to fix any
-    {_id: 1, name: 'React', priority: 'high'},
-    {_id: 2, name: 'anime', priority: 'low'},
-    {_id: 3, name: 'games', priority: 'low'},
-    {_id: 4, name: 'work', priority: 'high'},
-    {_id: 5, name: 'html & css', priority: 'middle'},
+const defaultAffairs: Array<AffairType> = [
+    { _id: 1, name: 'React', priority: 'high' },
+    { _id: 2, name: 'anime', priority: 'low' },
+    { _id: 3, name: 'games', priority: 'low' },
+    { _id: 4, name: 'work', priority: 'high' },
+    { _id: 5, name: 'html & css', priority: 'middle' },
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-    if (filter === 'all') return affairs
-    else return // need to fix
+
+export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { // тип параметров на выход желительно указывать
+    if (filter === 'all') return affairs // если входящие параметры с фильтра 'all' - рисуем все элементы
+    else return affairs.filter((e) => e.priority === filter) // иначе еще раз фильтруем и выводим то что получаем от filter
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-    return // need to fix
+
+export const deleteAffair = (affairs: Array<AffairType>, _id: number): Array<AffairType> => {
+    return affairs.filter(e => e._id !== _id) // проходим фильтром и и рисуем все, что не пришедший _id
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
-    const [filter, setFilter] = useState<FilterType>('all')
+    const [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs) // начальное состояние [ {} ]
+    const [filter, setFilter] = useState<FilterType>('all') // начальное состояние фильтра
 
-    const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => setAffairs(deleteAffair(affairs, _id)) // need to fix any
+    const filteredAffairs = filterAffairs(affairs, filter) // кладем в переменную вызванную F с начальными состояниями в параметры
+
+    const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id)) // не понял как работает....(
 
     return (
         <div>
-            <hr/>
+            <hr />
             homeworks 2
 
             {/*should work (должно работать)*/}
@@ -43,10 +50,10 @@ function HW2() {
                 deleteAffairCallback={deleteAffairCallback}
             />
 
-            <hr/>
+            <hr />
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeAffairs/>*/}
-            <hr/>
+            <hr />
         </div>
     )
 }
